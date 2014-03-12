ec203e684923129ad82e81ba6eac4cebfe54863b
Annie Cheung <annieccheung@gmail.com>
2014-03-12 17:08:52 +0800

    Add encrypt_password method on User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,20 @@ class User
   include Mongoid::Document 
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
+  before_save :encrypt_password
+
   field :email, type: String
-  field :salt, type: String
-  field :fish, type: String
+  field :tapatio, type: String
+  field :burrito, type: String
+
+  def encrypt_password
+  	puts "THIS IS HAPPENING BEFORE SAVE... woah....#{self.password}"
+  	if password.present?
+  		self.tapatio = BCrypt::Engine.generate_salt
+  		self.burrito = BCrypt::Engine.hash_secret(password, tapatio)
+  	end
+  end
 
 end
